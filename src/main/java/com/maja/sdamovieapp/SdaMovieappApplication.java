5d488b443ca6bf7525d88c3ad7c1ddab0c3f6cee
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-08-08 10:59:55 +0200

    first commit

---
+++ src/main/java/com/maja/sdamovieapp/SdaMovieappApplication.java
@@ -0,0 +1,13 @@
+package com.maja.sdamovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaMovieappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaMovieappApplication.class, args);
+    }
+
+}
