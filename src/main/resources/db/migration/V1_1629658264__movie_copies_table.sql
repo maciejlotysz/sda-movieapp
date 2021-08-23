a3241efdbf9fdd3b1dd291358225a9304ccf2270
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-08-23 22:59:46 +0200

    #151 optymalizacja skryptów sql

--- src/main/resources/db/migration/V1_1629658264__movie_copies_table.sql
+++ src/main/resources/db/migration/V1_1629658264__movie_copies_table.sql
@@ -1,8 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE movie_copies
 (
     id              BIGSERIAL PRIMARY KEY,
     movie_id        BIGINT not null,
-    copy_id         uuid DEFAULT uuid_generate_v4(),
+    copy_id         uuid unique DEFAULT uuid_generate_v4(),
     status          VARCHAR(20),
     disc_type       VARCHAR(20),
     CONSTRAINT movie_copies__movie_id_fk
