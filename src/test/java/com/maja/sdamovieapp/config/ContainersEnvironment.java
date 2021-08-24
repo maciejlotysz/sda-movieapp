2cc5081efea8270f3bfd93bee6b6133c9c615074
Maciej Lotysz <maciek.lotysz@gmail.com>
2021-08-24 12:42:23 +0200

    #162 Dodanie skryptu czyszczącego baze danych po każdym tescie

--- src/test/java/com/maja/sdamovieapp/config/ContainersEnvironment.java
+++ src/test/java/com/maja/sdamovieapp/config/ContainersEnvironment.java
@@ -1,11 +1,13 @@
 package com.maja.sdamovieapp.config;
 
 import com.maja.sdamovieapp.containers.PostgresTestContainer;
+import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:sql/delete.sql")
 public class ContainersEnvironment {
 
     @Container
---
+++ src/test/resources/sql/delete.sql
@@ -0,0 +1,11 @@
+DELETE FROM delivery_addresses;
+DELETE FROM movie_rates;
+DELETE FROM movie_copies;
+DELETE FROM movie_copy_orders;
+DELETE FROM orders;
+DELETE FROM users;
+DELETE FROM movies;
+
+
+
+
